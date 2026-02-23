b4448362bb8325edd1099f15dcd9fc4db02b22b0
Parth Vidya das <parth.das@omnenest.com>
2026-02-23 17:21:37 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Week2-Parth/', // add this the repo name
+   base:'/Week2-parth-das/', // add this the repo name
 })
